6e5e6f046c7fecc4dc12eb95a0ed106d4bf069ff
Mostafa Raihan <raihan.invite@gmail.com>
2026-03-10 21:59:51 +0600

    add useFefect"

---
+++ src/useFetch.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+        setError('');
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+        setUsers([]);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <>
+      {loading && <h3>Loading...</h3>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default UserList;
